86100a8773cd5a1803cda597aab3cd4660ede003
Wesley Miranda Novaes <wes.mnovaes@gmail.com>
2022-03-14 18:27:59 -0300

    Novo compomente e formulário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SenhaComponent } from './senha/senha.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: SenhaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
