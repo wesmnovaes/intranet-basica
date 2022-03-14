e29c9ebd36f73ff8175546841f0e4e245c953593
Wesley Miranda Novaes <wes.mnovaes@gmail.com>
2022-03-14 17:25:07 -0300

    Publicano diretório local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ss-web';
+  msg = 'Seja bem-vindo ao servidor simpro!';
+  title = 'Servidor Simpro - HMCR';
+  
+
+  ngOnInit(): void {
+    console.log('passei aqui')
+
+    const file = require('file-system');
+    file.readFile('./assets')
+    this.msg = 'Cheguei até aqui'
+
+  }
+
+
+
+
 }
