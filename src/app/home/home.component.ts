21fb8cb720fbeded06088ca667f21f61633e1677
unknown <wes.mnovaes@gmail.com>
2022-03-16 17:42:14 -0300

    Criando a interface dos posts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,15 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  teste = "A partir do dia 19/03, todos os acompanhantes terão de realizar o teste de covid-19 para poder acompanhar o(a) paciente internado</p>     A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>    A partir do dia 19/03 todos os acompanhantes terão de realizar o teste de covid-19 <br>";  
+  cards = [{titulo: 'Hey! Sou um título', foto: 'Essa é minha foto', conteudo: 'Eu sou o conteúdo dessa circular e estou pasando aqui para avisar que todos os funcionários receberão um aumento de 15% no salário e que mês que veem todos os funcionários poderão retirar um picolé diretamente do freezer da cantina! não perca sua chance de ganhar free diabetes!', data_criacao: '16/03/2022 16:42', autor: 'Maria Abadia'},
+          {titulo: 'Os avisos continuam', foto: 'Essa é minha foto', conteudo: 'Eu sou o conteúdo dessa circular e estou pasando aqui para avisar que todos os funcionários receberão um aumento de 15% no salário e que mês que veem todos os funcionários poderão retirar um picolé diretamente do freezer da cantina! não perca sua chance de ganhar free diabetes!', data_criacao: '16/03/2022 16:42', autor: 'Maria Abadia'},
+          {titulo: 'Os avisos continuam', foto: 'Essa é minha foto', conteudo: 'Eu sou o conteúdo dessa circular e estou pasando aqui para avisar que todos os funcionários receberão um aumento de 15% no salário e que mês que veem todos os funcionários poderão retirar um picolé diretamente do freezer da cantina! não perca sua chance de ganhar free diabetes!', data_criacao: '16/03/2022 16:42', autor: 'Maria Abadia'}
+]
   
   constructor() { }
 
     
 
-  ngOnInit(
-    
-  ): void {
+  ngOnInit(): void {
+
   }
 
 }
