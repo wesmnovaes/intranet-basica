fb74e14c39dd7d9e899d7bd744af8a8a812cad85
unknown <wes.mnovaes@gmail.com>
2022-03-17 11:18:31 -0300

    Finalizado front-end da página home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,9 +12,14 @@ export class HomeComponent implements OnInit {
           {titulo: 'Os avisos continuam', foto: 'Essa é minha foto', conteudo: 'Eu sou o conteúdo dessa circular e estou pasando aqui para avisar que todos os funcionários receberão um aumento de 15% no salário e que mês que veem todos os funcionários poderão retirar um picolé diretamente do freezer da cantina! não perca sua chance de ganhar free diabetes!', data_criacao: '16/03/2022 16:42', autor: 'Maria Abadia'}
 ]
 
-artigos = [{titulo: 'Como resetar impressora', autor: 'Kleber', autor_avatar: 'avatar_profile_w.png'},
-           {titulo: 'Calculando Hash', autor: 'Wesley', autor_avatar: 'avatar_profile_w.png'},
+avisos = [{titulo: 'Autorização: Mudança na Amil', autor: 'Maria Abadia', autor_avatar: 'avatar_profile_w.png'},
+           {titulo: 'Vestimenta Interna', autor: 'Ana Clara', autor_avatar: 'avatar_profile_w.png'},
            {titulo: 'Manual recepção', autor: 'Marina', autor_avatar: 'avatar_profile_w.png'},
+]
+
+artigos = [{titulo: 'Agendando Exames de Ultrassom', autor: 'Wesley', autor_avatar: 'folder_yellow_icon.png'},
+           {titulo: 'Trocando cilíndro Impressora Brother', autor: 'Ana Clara', autor_avatar: 'folder_yellow_icon.png'},
+           {titulo: 'Autorização Orizon', autor: 'Marina', autor_avatar: 'folder_yellow_icon.png'},
 ]
 
 autor_avatar: string = '../../assets/avatar_profile_w.png';
