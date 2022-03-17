26a162a9108bd551f41585827f42afb9b7a50739
unknown <wes.mnovaes@gmail.com>
2022-03-17 12:15:42 -0300

    Criando página de nova senha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { SenhaComponent } from './senha/senha.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { CrudSenhasComponent } from './senha/crud-senhas/crud-senhas.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
+  {path: '', component: HomeComponent},  
+  {path: 'login', component: LoginComponent},
   {path: 'senhas', component: SenhaComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'nova-senha', component: CrudSenhasComponent}
+  
 ];
 
 @NgModule({
