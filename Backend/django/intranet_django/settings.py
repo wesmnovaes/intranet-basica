1e5d119df5226b069380ac8b45f17c8eca19b2a0
Wesley Miranda Novaes <wes.mnovaes@gmail.com>
2022-07-22 09:48:42 -0300

    Ajuste na estrutura e configuração do nginx + django

--- Backend/django/intranet_django/settings.py
+++ Backend/django/intranet_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-1s9r*4n_@&!f8vpw$1(xk1ysy_ic8jtjn@#p=dt()e8by$37w-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django','localhost']
+ALLOWED_HOSTS = ['django','localhost','intranet']
 
 CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
@@ -74,16 +75,16 @@ WSGI_APPLICATION = 'intranet_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    },
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'intranet',
         'USER': 'root',
         'PASSWORD': 'hmcr2020',
-        'HOST': 'database',
+        'HOST': '172.18.0.2',
         'PORT': '',
     }
 }
@@ -123,7 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-python manage.py makemigrations --no-input
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
-#python manage.py runserver
-#gunicorn howtodjango.wsgi:application --bind 0.0.0.0:8000
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+python manage.py runserver
+gunicorn howtodjango.wsgi:application --bind 0.0.0.0:8000
